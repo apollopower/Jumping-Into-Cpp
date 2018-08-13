6159da313dd43610e9f7380fa1d4e73291589915
Jonas Erthal <erthaljonas@gmail.com>
2018-08-13 18:34:57 -0400

    chapter 2

---
+++ chap_2/p3.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+int main() {
+    float num1;
+    float num2;
+
+    cout << "Please input your first number: ";
+    cin >> num1;
+    cout << "Please input your second number: ";
+    cin >> num2;
+    cout << "Your numbers divided is equal to: " << num1 / num2 << "\n";
+}
